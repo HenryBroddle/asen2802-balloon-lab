be92777c2de7f4dfd036bfc30dc7ba9666e47184
AntonioJF1 <anji9940@colorado.edu>
2024-09-20 11:31:55 -0600

    Update Balloon.m

--- Balloon.m
+++ Balloon.m
@@ -11,7 +11,7 @@ classdef Balloon
         function obj = Balloon(diameter, depth, substrate_mass, fixture_mass, payload_mass, fluid_mass)
             obj.mass = fixture_mass + substrate_mass + payload_mass + fluid_mass;
             obj.volume = 4/3 * pi * depth/2 * (diameter/2)^2;
-        end
+        end %as;ldkfj
     end
 end
 
