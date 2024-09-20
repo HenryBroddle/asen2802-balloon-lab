f901cbde25a5472e31b9c96c1221d7ddff6bf0b4
AntonioJF1 <anji9940@colorado.edu>
2024-09-20 12:09:22 -0600

    added volume variable

--- Balloon.m
+++ Balloon.m
@@ -8,11 +8,11 @@ classdef Balloon
     end
     
     methods
-        function obj = Balloon(diameter, depth, substrate_mass, fixture_mass, payload_mass, fluid_mass) % *(m, m, g, g, g, g)
+        function obj = Balloon(volume, substrate_mass, fixture_mass, payload_mass, fluid_mass) % *(m^3, g, g, g, g)
             % substrate = balloon material mass, fixture = added mass
             % (string and clip), payload mass = mass of paperclips
             obj.mass = fixture_mass + substrate_mass + payload_mass + fluid_mass;
-            obj.volume = 4/3 * pi * depth/2 * (diameter/2)^2;
+            obj.volume = volume;
         end
     end
 end
