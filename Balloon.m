76a9e531116f771a9eb6cb707b7d9d484f5ae760
AntonioJF1 <anji9940@colorado.edu>
2024-09-20 11:43:29 -0600

    added units

--- Balloon.m
+++ Balloon.m
@@ -8,7 +8,7 @@ classdef Balloon
     end
     
     methods
-        function obj = Balloon(diameter, depth, substrate_mass, fixture_mass, payload_mass, fluid_mass)
+        function obj = Balloon(diameter, depth, substrate_mass, fixture_mass, payload_mass, fluid_mass) % *(m, m, g, g, g, g)
             obj.mass = fixture_mass + substrate_mass + payload_mass + fluid_mass;
             obj.volume = 4/3 * pi * depth/2 * (diameter/2)^2;
         end
