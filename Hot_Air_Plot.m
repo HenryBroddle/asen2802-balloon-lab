90944194173c665009afded8a11aab72b3177918
AntonioJF1 <anji9940@colorado.edu>
2024-09-27 12:08:57 -0600

    Stash hot-air changes

---
+++ Hot_Air_Plot.m
@@ -0,0 +1,28 @@
+clc; close; clear all;
+
+R = 0.287; % Gas Constant of air (KJ / (kg * K))
+m_Balloon = 0.0273; % kg
+V_Balloon = 0.113; % m^3
+
+Altitude = linspace(1,10500,10500);
+[T_Env,a,P_Env,Rho_Env] = atmoscoesa(Altitude);
+
+T_Balloon = ((1 ./ T_Env) - ((R .* m_Balloon) ./ ((P_Env ./ 1000) .* V_Balloon))) .^ -1;
+
+plot(Altitude, T_Balloon)
+xlabel('Altitude [m]')
+ylabel('Temperature of Balloon [K]')
+title('Temperature of  balloon vs altitude')
+hold on
+
+Max_Altitude = 0;
+counter = 0;
+for i = 1:10500
+    if(T_Balloon(i) > 522 && counter == 0)
+        Max_Altitude = i;
+        counter = counter + 1;
+    end
+end
+
+xline(Max_Altitude, '--b')
+yline(522, '--r')
